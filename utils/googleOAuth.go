dba48adf85291eba087749e6d93e82df86d133ed
Theerachot <theerachot-tle@hotmail.com>
2023-09-20 19:50:45 +0700

    get oauth token and user information

---
+++ utils/googleOAuth.go
@@ -0,0 +1,130 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/Jellicle-Cats/user-service/config"
+)
+
+type GoogleOauthToken struct {
+	AccessToken string
+	IdToken     string
+}
+
+type GoogleUserResult struct {
+	Id             string
+	Email          string
+	Verified_email bool
+	Name           string
+	Given_name     string
+	Family_name    string
+	Picture        string
+	Locale         string
+}
+
+func GetGoogleOauthToken(code string) (*GoogleOauthToken, error) {
+	const rootURL = "https://oauth2.googleapis.com/token"
+
+	config := config.ReadConfig()
+	values := url.Values{}
+	values.Add("grant_type", "authorization_code")
+	values.Add("code", code)
+	values.Add("client_id", config.ClientID)
+	values.Add("client_secret", config.ClientSecret)
+	values.Add("redirect_uri", config.RedirectURL)
+
+	query := values.Encode()
+
+	req, err := http.NewRequest("POST", rootURL, bytes.NewBufferString(query))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("could not retrieve token")
+	}
+
+	var resBody bytes.Buffer
+	_, err = io.Copy(&resBody, res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var GoogleOauthTokenRes map[string]interface{}
+
+	if err := json.Unmarshal(resBody.Bytes(), &GoogleOauthTokenRes); err != nil {
+		return nil, err
+	}
+
+	tokenBody := &GoogleOauthToken{
+		AccessToken: GoogleOauthTokenRes["access_token"].(string),
+		IdToken:     GoogleOauthTokenRes["id_token"].(string),
+	}
+
+	return tokenBody, nil
+}
+
+func GetGoogleUser(access_token string, id_token string) (*GoogleUserResult, error) {
+	rootUrl := fmt.Sprintf("https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=%s", access_token)
+
+	req, err := http.NewRequest("GET", rootUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", id_token))
+
+	client := http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("could not retrieve user")
+	}
+
+	var resBody bytes.Buffer
+	_, err = io.Copy(&resBody, res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var GoogleUserRes map[string]interface{}
+
+	if err := json.Unmarshal(resBody.Bytes(), &GoogleUserRes); err != nil {
+		return nil, err
+	}
+
+	userBody := &GoogleUserResult{
+		Id:             GoogleUserRes["id"].(string),
+		Email:          GoogleUserRes["email"].(string),
+		Verified_email: GoogleUserRes["verified_email"].(bool),
+		Name:           GoogleUserRes["name"].(string),
+		Given_name:     GoogleUserRes["given_name"].(string),
+		Picture:        GoogleUserRes["picture"].(string),
+		Locale:         GoogleUserRes["locale"].(string),
+	}
+
+	return userBody, nil
+}
